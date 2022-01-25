8528c61b5bdddb8d363b5a0e8d19544cb4e9b2ad
mmelendez3 <mele0019@gmail.com>
2022-01-25 16:35:53 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mern Group Project 3 
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
