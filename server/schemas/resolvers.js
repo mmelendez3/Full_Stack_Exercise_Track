d3c753491b1898dbf9a4b5ece075578620ea241d
mmelendez3 <mele0019@gmail.com>
2022-01-26 13:25:30 -0600

    added typeDefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,69 @@
+const { User, Exercise } = require("../models");
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
+      me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id })
+            .select('-__v -password')
+            .populate('exercises');
+      
+          return userData;
+        }
+      
+        throw new AuthenticationError('Not logged in');
+      },
+    //   exercises: async (parent, { username }) => {
+    //     const params = username ? { username } : {};
+    //     return Exercise.find(params).sort({ createdAt: -1 });
+    //   },
+
+    //get exercise by ID
+      exercise: async (parent, { _id }) => {
+        return Exercise.findOne({ _id });
+      },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+    
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
       }
-    }
-  };
+    
+      const correctPw = await user.isCorrectPassword(password);
+    
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+    
+      const token = signToken(user);
+  return { token, user };
+    },
+    addExercise: async (parent, args, context) => {
+        if (context.user) {
+          const exercise = await Exercise.create({ ...args, username: context.user.username });
+      
+          await User.findByIdAndUpdate(
+            { _id: context.user._id },
+            { $push: { exercises: exercise._id } },
+            { new: true }
+          );
+      
+          return exercise;
+        }
+      
+        throw new AuthenticationError('You need to be logged in!');
+      },
+},
+};
   
   module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,9 +3,38 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-type Query {
-    helloWorld: String
-  }`;
+type Exercise {
+    _id: ID
+    description: String
+    date: String
+    username: String
+    duration: Int
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    exercises: [Exercise]
+  }
+    type Query {
+      me: User
+      users: [User]
+      user(username: String!): User
+      exercises(username: String): [Exercise]
+      exercise(_id: ID!): Exercise
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+      addUser(username: String!, email: String!, password: String!): Auth
+      addExercise(description: String!): User
+      removeExercise(description: String!): User
+      }
+      type Auth {
+        token: ID!
+        user: User
+      }
+
+`;
 
 // export the typeDefs
 module.exports = typeDefs;
