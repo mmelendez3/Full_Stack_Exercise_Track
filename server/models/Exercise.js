db5b17594efb9577637a01db6634e7b91c7af1f7
mmelendez3 <mele0019@gmail.com>
2022-01-25 20:06:59 -0600

    added exercise model

---
+++ server/models/Exercise.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const exerciseSchema = new Schema(
+  {
+    description: {
+      type: String,
+      required: 'You need to leave a description!',
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    duration: {
+      type: Number,
+      required: true
+    }
+    
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+
+const Exercise = model('Exercise', exerciseSchema);
+
+module.exports = Exercise;
