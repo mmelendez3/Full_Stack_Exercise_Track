1d7533fab781e0ee889ddb175b6ce6aadf9e9227
raymondcerneytech <cerne004@gmail.com>
2022-01-28 14:15:27 -0600

    modified addExercise mutation in typeDef and resolvers, working on removeExercise

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,39 +1,38 @@
 // import the gql tagged template function
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
-type Exercise {
-    _id: ID
-    description: String
-    date: String
-    username: String
-    duration: Int
-  }
-  type User {
-    _id: ID
-    username: String
-    email: String
-    exercises: [Exercise]
-  }
-    type Query {
-      me: User
-      users: [User]
-      user(username: String!): User
-      exercises(username: String): [Exercise]
-      exercise(_id: ID!): Exercise
-    }
-    type Mutation {
-        login(email: String!, password: String!): Auth
-      addUser(username: String!, email: String!, password: String!): Auth
-      addExercise(description: String!): User
-      removeExercise(description: String!): User
-      }
-      type Auth {
-        token: ID!
-        user: User
-      }
-
+	type User {
+		_id: ID
+		username: String
+		email: String
+		exercises: [Exercise]
+	}
+	type Exercise {
+		_id: ID
+		description: String
+		date: String
+		username: String
+		duration: Int
+	}
+	type Query {
+		me: User
+		users: [User]
+		user(username: String!): User
+		exercises(username: String): [Exercise]
+		exercise(_id: ID!): Exercise
+	}
+	type Mutation {
+		login(email: String!, password: String!): Auth
+		addUser(username: String!, email: String!, password: String!): Auth
+		addExercise(description: String!, date: String!, username: String!, duration: String!): User
+		removeExercise(_Id: ID!): User
+	}
+	type Auth {
+		token: ID!
+		user: User
+	}
 `;
 
 // export the typeDefs
