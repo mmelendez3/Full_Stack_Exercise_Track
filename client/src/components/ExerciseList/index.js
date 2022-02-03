1496c09621c630fb2bd0916bb11dabfb32e2f070
EmilyT <emilython@hotmail.com>
2022-02-02 20:33:10 -0600

    added delete div onclicl

--- client/src/components/ExerciseList/index.js
+++ client/src/components/ExerciseList/index.js
@@ -1,35 +1,20 @@
 import React from 'react';
-import { REMOVE_EXERCISE } from '../../utils/mutations';
-import { useMutation } from '@apollo/client';
-import Auth from '../../utils/auth';
-import { removeExerciseId } from '../../utils/localStorage';
+// import { REMOVE_EXERCISE } from '../../utils/mutations';
+// import { useMutation } from '@apollo/client';
+// import Auth from '../../utils/auth';
+// import { removeExerciseId } from '../../utils/localStorage';
 
 const ExerciseList = ({ exercises, description }) => {
-  const [removeExercise] = useMutation(REMOVE_EXERCISE);
+  // const [removeExercise] = useMutation(REMOVE_EXERCISE);
 
   if (!exercises.length) {
     return <h3>No exercises logged</h3>;
   }
 
-  // const handleRemoveExercise = async (exerciseId) => {
-  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
-  //   console.log(exerciseId);
-  //   if (!token) {
-  //     return false;
-  //   }
-
-  //   try {
-  //     // Apollo will cache the response and automatically refetch and update
-  //     await removeExercise({
-  //       variables: { exerciseId: exerciseId },
-  //     });
-
-  //     // upon success, remove book's id from localStorage
-  //     removeExerciseId(exerciseId);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
+  const clickHandler = () => {
+    const exerciseDiv = document.getElementById('exerciseDiv');
+    exerciseDiv.remove();
+  };
 
   return (
     <div>
@@ -42,14 +27,7 @@ const ExerciseList = ({ exercises, description }) => {
               for {exercise.duration} minutes, logged on {exercise.date}
             </p>
             <button>Edit</button>
-            <button
-              onClick={() => {
-                removeExercise({ variables: { id: exercise.id } });
-                window.location.reload();
-              }}
-            >
-              Delete
-            </button>
+            <button onClick={clickHandler}>Delete</button>
           </div>
         ))}
     </div>
@@ -58,4 +36,31 @@ const ExerciseList = ({ exercises, description }) => {
 
 export default ExerciseList;
 
-//onClick={() => handleRemoveExercise(exercise.exerciseId)}
+// Trying out MERN REMOVE_EXERCISE
+// ==================================================
+
+// MERN ONCLICK
+// onClick={() => {
+//   removeExercise({ variables: { id: exercise.id } });
+//   window.location.reload();
+// }}
+
+// const handleRemoveExercise = async (exerciseId) => {
+//   const token = Auth.loggedIn() ? Auth.getToken() : null;
+//   console.log(exerciseId);
+//   if (!token) {
+//     return false;
+//   }
+
+//   try {
+//     // Apollo will cache the response and automatically refetch and update
+//     await removeExercise({
+//       variables: { exerciseId: exerciseId },
+//     });
+
+//     // upon success, remove book's id from localStorage
+//     removeExerciseId(exerciseId);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
